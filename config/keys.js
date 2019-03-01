aff9bf9688e4f2bf0c73123e5b9d9c28d059834f
ravikirany711 <ravikirany711@gmail.com>
2019-03-01 11:47:22 +0530

    keys added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI:'mongodb://ravi123:ravi123@storybooks-shard-00-00-30y4x.mongodb.net:27017,storybooks-shard-00-01-30y4x.mongodb.net:27017,storybooks-shard-00-02-30y4x.mongodb.net:27017/test?ssl=true&replicaSet=storybooks-shard-0&authSource=admin&retryWrites=true',
+    googleClientID:'671492137792-ciltmcilktkknos46rfrgk78j2ka9hns.apps.googleusercontent.com',
+    googleClienSecret:'gyavxHUOaEsEKuyCa8fS15BG'
+}
