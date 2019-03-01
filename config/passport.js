9d86ec82de47655e36874a9ddb0369793075b170
ravikirany711 <ravikirany711@gmail.com>
2019-03-01 13:07:16 +0530

    redirecting to google accounts loading error introduced

--- config/passport.js
+++ config/passport.js
@@ -10,8 +10,8 @@ module.exports=function(passport){
             callbackURL:'/auth/google/callback',
             proxy:true
         },(accessToken,refreshToken,profile,done)=>{
-            console.log('accessToken')
-            console.log('profile')
+            console.log(accessToken)
+            console.log(profile)
 
 
         })
--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,12 @@ router.get('/google',passport.authenticate('google',{
     scope:['profile','email']
 }))
 
+router.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res)=> {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
 
 
 
